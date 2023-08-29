a72ba33f79544bc4615dd5a8f95974ed87de748d
pettlasuresh <143484365+pettlasuresh@users.noreply.github.com>
2023-08-29 12:12:52 +0530

    Add files via upload

---
+++ djproject/userapp/tests.py
@@ -0,0 +1,24 @@
+<!DOCTYPE htmL>
+<ntml>
+  <head>
+    <title>Hi DUde its a Ed Notes For All</title>
+    <link href="(% staic 'style.css'%)"
+rel="styleheet">
+
+  </head>
+<body style="background-color .rgb(255, 153, 0);">
+<center>
+ <img src={% static ."kd.jpg'%}'kd"
+style="border.5px soild red",>
+  <hl>Hi dude its Ed Notes for all</hl>
+
+
+
+<a href="/users" style="fort-size"
+
+
+
+
+
+
+
